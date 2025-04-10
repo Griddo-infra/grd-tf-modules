beab3e85e9f55b8ee882fa0fb6d6dc7365174759
Orencio Ramirez <orencio.ramirez@secuoyas.com>
2025-04-10 14:50:00 +0200

    Update main.tf

--- modules/eks-karpenter/main.tf
+++ modules/eks-karpenter/main.tf
@@ -1,6 +1,6 @@
 module "karpenter" {
   source                 = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version                = "20.33.5"
+  version                = "20.35.0"
   cluster_name           = var.cluster_name
   irsa_oidc_provider_arn = var.cluster_oidc_provider_arn
   enable_irsa            = true
